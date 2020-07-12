7d2c70b1f5489cc11e5e3b7fadeee2d818428432
AndreaCossu <andrea.cossu@sns.it>
2020-07-12 17:55:40 +0200

    Minor change

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -1,8 +1,8 @@
 import torch
+import clutils
 from clutils.models import MLP
-from clutils.globals import OUTPUT_TYPE
 
 mlp = MLP(3, [10, 20], 'cpu', 7)
-mlp.output_type = OUTPUT_TYPE.OUTH
+mlp.output_type = clutils.OUTPUT_TYPE.OUTH
 out = mlp(torch.randn(2, 3))
 print(out)
