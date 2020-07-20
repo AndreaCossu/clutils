42195a8801700b4a09c68d8c95e4fd1c8733397c
AndreaCossu <andrea.cossu@sns.it>
2020-07-20 16:15:32 +0200

    Changed test

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -3,6 +3,6 @@ import clutils
 from clutils.models import MLP
 
 mlp = MLP(3, [10, 20], 'cpu', 7)
-mlp.output_type = clutils.OUTPUT_TYPE.OUTH
+mlp.output_type = clutils.OUTPUT_TYPE.LAST_OUT
 out = mlp(torch.randn(2, 3))
 print(out)
