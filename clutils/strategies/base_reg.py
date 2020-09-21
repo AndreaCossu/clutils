1785119ad1cd3942883ccbe4feebed439724613e
AndreaCossu <andrea.cossu@sns.it>
2020-09-21 12:33:28 +0200

    Added average clip

--- clutils/strategies/base_reg.py
+++ clutils/strategies/base_reg.py
@@ -15,7 +15,8 @@ class BaseReg():
         :param single_batch: if True compute fisher by averaging gradients pattern by pattern. 
                 If False, compute fisher by averaging mini batches.
         :param cumulative: Keep a single penalty matrix for all tasks or one for each task.
-                Possible values are 'none' or 'sum'.
+                Possible values are 'none' or 'sum', 'avgclip'.
+                'avgclip' = clip((F_{i-1} + F_i) / i, max_f)
                 Keep one separate penalty for each task if 'none'. 
 
         '''
@@ -25,12 +26,14 @@ class BaseReg():
         self.device = device
         self.normalize = normalize
         self.single_batch = single_batch
-        assert(cumulative == 'none' or cumulative == 'sum')
+        assert(cumulative == 'none' or cumulative == 'sum' or cumulative == 'avgclip')
         self.cumulative = cumulative
 
         self.saved_params = defaultdict(list)
         self.importance = defaultdict(list)
 
+        self.max_clip = 0.8
+        self.wi = {0: 0.00015, 1: 0.000005, 2: 0.000005, 3: 0.000005, 4: 0.000005}
 
     def penalty(self, current_task_id):
         '''
@@ -47,7 +50,7 @@ class BaseReg():
                 for (_, param), (_, saved_param), (_, imp) in zip(self.model.named_parameters(), self.saved_params[task], self.importance[task]):
                     pad_difference = padded_op(param, saved_param)
                     total_penalty += (padded_op(imp, pad_difference.pow(2), op='*')).sum()
-        elif self.cumulative == 'sum' and current_task_id > 0:
+        elif (self.cumulative == 'sum' or self.cumulative == 'avgclip') and current_task_id > 0:
             for (_, param), (_, saved_param), (_, imp) in zip(self.model.named_parameters(), self.saved_params[current_task_id], self.importance[current_task_id]):
                 pad_difference = padded_op(param, saved_param)
                 total_penalty += (padded_op(imp, pad_difference.pow(2), op='*')).sum()            
@@ -60,13 +63,14 @@ class BaseReg():
         # no need to store all the tensor metadata, just its data (data.clone())
         self.saved_params[current_task_id] = copy_params_dict(self.model)
 
-    def update_importance(self, current_task_id, importance):
+    def update_importance(self, current_task_id, importance, save_pars=True):
         '''
         :param current_task_id: current task ID (0 being the first task)
         :importance : importance for each weight
         '''
         
-        self.save_old_parameters(current_task_id)
+        if save_pars:
+            self.save_old_parameters(current_task_id)
  
         if self.cumulative == 'none' or current_task_id == 0:
             self.importance[current_task_id] = importance
@@ -76,3 +80,10 @@ class BaseReg():
             for (k1,curr_imp),(k2,imp) in zip(self.importance[current_task_id-1], importance):
                 assert(k1==k2)
                 self.importance[current_task_id].append( (k1, padded_op(imp, curr_imp, op='+')) )
+        elif self.cumulative == 'avgclip' and current_task_id > 0:
+            for (k1,curr_imp),(k2,imp) in zip(self.importance[current_task_id-1], importance):
+                assert(k1==k2)
+                self.importance[current_task_id].append( (
+                    # self.wi[current_task_id]*curr_imp
+                    k1, (padded_op(imp, curr_imp, op='+') / float(current_task_id+1)).clamp(max=self.max_clip)
+                ))
